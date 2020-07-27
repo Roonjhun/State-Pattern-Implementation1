018aef6f4472c633502263a9d7f6b2a0d7173a31
Roonjhun <44239877+Roonjhun@users.noreply.github.com>
2020-07-27 17:15:09 +0200

    Update State5_2.java

--- src/labTask/State5_2.java
+++ src/labTask/State5_2.java
@@ -8,7 +8,6 @@ public class State5_2 extends State3 {
 	}
 	
 	public void Operation7(StateMachineContext context) throws Exception{
-		history = null;
 		context.getCurrentState().CurrentStatesRegion[1] = null;
 		System.out.println("Final state of Region 2 reached.");
 	}
